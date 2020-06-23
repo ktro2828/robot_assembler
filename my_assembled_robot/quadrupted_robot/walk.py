fe9cdf1fefb2583b8da032be7384e253342ee295
kotarouetake <ktro310115@gmail.com>
2020-06-24 02:22:16 +0900

    Update walk.py

--- my_assembled_robot/quadrupted_robot/walk.py
+++ my_assembled_robot/quadrupted_robot/walk.py
@@ -14,7 +14,7 @@ def scan_callback(msg):
 # initial value of g_range_ahead
 g_range_ahead = 1
 
-rospy.init_node('send_motion')
+rospy.init_node('send_motion', anonymous=T)
 scan_sub = rospy.Subscriber('/range_sensor', LaserScan, scan_callback)
 act_client = actionlib.SimpleActionClient('/fullbody_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
 
@@ -22,27 +22,21 @@ act_client.wait_for_server()
 
 rate = rospy.Rate(10)
 start = rospy.Time.now()
-while (1):
+while not rospy.is_shutdown():
     # gen msg
     traj_msg = FollowJointTrajectoryGoal()
     traj_msg.trajectory.header.stamp = rospy.Time.now() + rospy.Duration(0.2)
     traj_msg.trajectory.joint_names = ['JOINT0', 'JOINT1', 'JOINT2', 'JOINT3', 'JOINT4', 'JOINT5', 'JOINT6', 'JOINT7']
 
 
-    traj_msg.trajectory.points.append(JointTrajectoryPoint(positions=[-0.55, 0.4, 0.1, -0.4, 0.1, 0.4, -0.55, -0.4], #姿勢1
-                                                           time_from_start = rospy.Duration(1))) ## 前の姿勢から2se
-    traj_msg.trajectory.points.append(JointTrajectoryPoint(positions=[0.1, 0.4, -0.55, -0.4, -0.55, 0.4, 0.1, -0.4], #姿勢2
-                                                           time_from_start = rospy.Duration(2)))## 前の姿勢から4sec
-    traj_msg.trajectory.points.append(JointTrajectoryPoint(positions=[-0.55, 0.4, 0.1, -0.4, 0.1, 0.4, -0.55, -0.4], #姿勢1
-                                                        time_from_start = rospy.Duration(3))) ## 前の姿勢から2se
-    traj_msg.trajectory.points.append(JointTrajectoryPoint(positions=[0.1, 0.4, -0.55, -0.4, -0.55, 0.4, 0.1, -0.4], #姿勢2
-                                                            time_from_start = rospy.Duration(4)))## 前の姿勢から4sec
-    '''
-    traj_msg.trajectory.points.append(JointTrajectoryPoint(positions=[-0., 0, 0.7, 0, 0, 0, 0, 0], #姿勢3
-                                                           time_from_start = rospy.Duration(10)))## 前の姿勢から4sec
-    traj_msg.trajectory.points.append(JointTrajectoryPoint(positions=[0.7, 0, 0.7, 0, -0.55, 0, -0.55, 0], #姿勢4
-                                                           time_from_start = rospy.Duration(12)))
-    '''
+    traj_msg.trajectory.points.append(JointTrajectoryPoint(positions=[-0.55, 0.4, 0.1, -0.4, 0.1, 0.4, -0.55, -0.4],
+                                                           time_from_start = rospy.Duration(1)))
+    traj_msg.trajectory.points.append(JointTrajectoryPoint(positions=[0.1, 0.4, -0.55, -0.4, -0.55, 0.4, 0.1, -0.4],
+                                                           time_from_start = rospy.Duration(2)))
+    traj_msg.trajectory.points.append(JointTrajectoryPoint(positions=[-0.55, 0.4, 0.1, -0.4, 0.1, 0.4, -0.55, -0.4],
+                                                        time_from_start = rospy.Duration(3)))
+    traj_msg.trajectory.points.append(JointTrajectoryPoint(positions=[0.1, 0.4, -0.55, -0.4, -0.55, 0.4, 0.1, -0.4],
+                                                            time_from_start = rospy.Duration(4)))
 
     # send to robot
     act_client.send_goal(traj_msg)
@@ -50,5 +44,3 @@ while (1):
     act_client.wait_for_result()
 
     rospy.loginfo("done")
-    if g_range_ahead < 0.8:
-        break
